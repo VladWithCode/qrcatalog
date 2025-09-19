21eedf65dd9f7405263426c407cd9404e201a7c3
vladwb <vladwithcode@gmail.com>
2025-09-19 14:44:34 -0600

    fix: page wrapper className will be replaced if present in calling component

--- frontend/src/components/pageWrapper.tsx
+++ frontend/src/components/pageWrapper.tsx
@@ -8,11 +8,11 @@ export const PageWrapper = forwardRef(
     ) => {
         return (
             <div
+                {...props}
                 className={cn(
                     "relative z-0 grid grid-rows-[auto_1fr_auto] h-screen w-screen overflow-x-hidden scroll-smooth",
                     props.className,
                 )}
-                {...props}
                 ref={ref}
             >
                 {props.children}
