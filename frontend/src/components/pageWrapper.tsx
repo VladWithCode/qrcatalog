b0163a517fce0ea6ea356e5ccf8dfb480dc763f8
vladwb <vladwithcode@gmail.com>
2025-09-19 13:47:49 -0600

    fix: website has no scroll
    Due to the wrapper not having a fixed height, the website has no scroll
    as the overflow is not handled correctly.

--- frontend/src/components/pageWrapper.tsx
+++ frontend/src/components/pageWrapper.tsx
@@ -1,13 +1,22 @@
 import { cn } from "@/lib/utils";
 import { forwardRef, type PropsWithChildren } from "react";
 
-export const PageWrapper = forwardRef((props: PropsWithChildren & React.HTMLAttributes<HTMLDivElement>, ref: React.Ref<HTMLDivElement>) => {
-    return (
-        <div className={cn(
-            "relative z-0 grid grid-rows-[auto_1fr_auto] w-screen overflow-hidden",
-            props.className,
-        )} {...props} ref={ref}>
-            {props.children}
-        </div>
-    );
-});
+export const PageWrapper = forwardRef(
+    (
+        props: PropsWithChildren & React.HTMLAttributes<HTMLDivElement>,
+        ref: React.Ref<HTMLDivElement>,
+    ) => {
+        return (
+            <div
+                className={cn(
+                    "relative z-0 grid grid-rows-[auto_1fr_auto] h-screen w-screen overflow-x-hidden scroll-smooth",
+                    props.className,
+                )}
+                {...props}
+                ref={ref}
+            >
+                {props.children}
+            </div>
+        );
+    },
+);
