33243dcd8c0d2778d46a46a6230f1562cbb9047d
vladwb <vladwithcode@gmail.com>
2025-09-19 16:28:42 -0600

    feat: properly handle cors

--- internal/routes/customServeMux.go
+++ internal/routes/customServeMux.go
@@ -1,6 +1,9 @@
 package routes
 
-import "net/http"
+import (
+	"net/http"
+	"os"
+)
 
 // customServeMux builds on top of http.ServeMux to provide the ability to customize
 // the handler for not found routes
@@ -23,17 +26,36 @@ func NewCustomServeMux() *customServeMux {
 // Will search for the handler appropiate for the received request, if found
 // processes the request normally, otherwise responds with a 404
 func (csm *customServeMux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	_, pattern := csm.Handler(r)
+	csm.setCORSHeaders(w)
+
+	if r.Method == http.MethodOptions {
+		w.WriteHeader(http.StatusNoContent)
+		return
+	}
+
+	handler, pattern := csm.Handler(r)
 
 	if pattern == "" {
 		csm.notFoundHandle(w, r)
 		return
 	}
 
-	csm.ServeMux.ServeHTTP(w, r)
+	handler.ServeHTTP(w, r)
 }
 
 // Set the custom NotFoundHandler
 func (csm *customServeMux) NotFoundHandleFunc(handler http.HandlerFunc) {
 	csm.notFoundHandle = handler
+}
+
+func (csm *customServeMux) setCORSHeaders(w http.ResponseWriter) {
+	corsAllowOrigin := os.Getenv("CORS_ALLOW_ORIGIN")
+	if corsAllowOrigin == "" {
+		corsAllowOrigin = "*"
+	}
+
+	w.Header().Set("Access-Control-Allow-Origin", corsAllowOrigin)
+	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+	w.Header().Set("Access-Control-Allow-Credentials", "true")
 }
