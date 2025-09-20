cfee340c576c0de974c514325bdf133e27e9706f
vladwb <vladwithcode@gmail.com>
2025-09-19 18:23:02 -0600

    chore: sticky dashboard header

--- frontend/src/components/dashboard/header.tsx
+++ frontend/src/components/dashboard/header.tsx
@@ -2,8 +2,8 @@ import { SidebarTrigger } from "../ui/sidebar";
 
 export function Header() {
     return (
-        <div className="relative z-30 w-full px-0.5 py-1">
-            <header className="flex items-center gap-6 justify-between p-2 rounded-lg shadow bg-gray-100">
+        <div className="sticky top-0 inset-x-0 z-30 w-full px-0.5 py-1">
+            <header className="relative flex items-center gap-6 justify-between p-2 rounded-lg shadow bg-gray-100">
                 <SidebarTrigger />
                 <h2 className="font-semibold text-gray-700 flex-1">
                     Panel de control
